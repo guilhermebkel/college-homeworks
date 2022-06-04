fcfa89deb97ec493c501a708c6e3b0fa53ae9235
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-06-04 16:13:38 -0300

    chore(*): modularize code

--- data-structures/TP1/TP/include/poker-face-util.h
+++ data-structures/TP1/TP/include/poker-face-util.h
@@ -3,6 +3,12 @@
 
 #include "poker-face.h"
 
+enum TieResult {
+	OVERWRITE_WINNER = 1,
+	ADD_WINNER = 2,
+	NO_TIE_FOUND = 3
+};
+
 int getBalanceSortingParam (Balance balance);
 int getCardComboSortingParam (CardCombo cardCombo);
 int getCardSortingParam (Card card);
@@ -10,5 +16,6 @@ GroupedCardCombo groupCardsWithEqualValues (Hand hand);
 int getCardComboScore (CardCombo cardCombo);
 Card getGreaterCard (Hand hand);
 ClassifiedHand classifyHand (Hand hand);
+TieResult assertTie (RoundWinner currentWinner, Play possibleWinnerPlay);
 
 #endif
--- data-structures/TP1/TP/include/poker-face-validation.h
+++ data-structures/TP1/TP/include/poker-face-validation.h
@@ -4,14 +4,7 @@
 #include "poker-face.h"
 #include "arrangement-list.h"
 
-enum TieResult {
-	OVERWRITE_WINNER = 1,
-	ADD_WINNER = 2,
-	NO_TIE_FOUND = 3
-};
-
 bool handHasClassificationType (Hand hand, ClassifiedHandType classifiedHandType);
 bool isInvalidPlay (Round round, ArrangementList<Balance> *balances);
-TieResult assertTie (RoundWinner currentWinner, Play possibleWinnerPlay);
 
 #endif
