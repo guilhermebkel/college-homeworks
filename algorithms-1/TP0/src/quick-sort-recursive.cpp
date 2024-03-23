88c9e4377eceb489a4e0d9748ce9a4b825c0019c
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-03-23 19:42:29 +0000

    chore(tp0): improve code style

--- algorithms-1/TP0/src/quick-sort-recursive.cpp
+++ algorithms-1/TP0/src/quick-sort-recursive.cpp
@@ -7,18 +7,18 @@ void quickSortRecursiveOrder (int left, int right, int itens[MAX_ARRAY_SIZE], in
     } else {
         int i, j;
 
-        quickSortPartition(left, right, &i, &j, itens,makeComparison);
+        quickSortPartition(left, right, &i, &j, itens, makeComparison);
 
         if (left < j) {
-            quickSortRecursiveOrder(left, j, itens, quickSortMaxPartitionSize,makeComparison);
+            quickSortRecursiveOrder(left, j, itens, quickSortMaxPartitionSize, makeComparison);
         }
 
         if (i < right) {
-            quickSortRecursiveOrder(i, right, itens, quickSortMaxPartitionSize,makeComparison);
+            quickSortRecursiveOrder(i, right, itens, quickSortMaxPartitionSize, makeComparison);
         }
     }
 }
 
 void quickSortRecursive (int itens[MAX_ARRAY_SIZE], int size, int quickSortMaxPartitionSize, bool (*makeComparison)(int, int)) {
-    quickSortRecursiveOrder(0, size - 1, itens, quickSortMaxPartitionSize,makeComparison);
+    quickSortRecursiveOrder(0, size - 1, itens, quickSortMaxPartitionSize, makeComparison);
 }
