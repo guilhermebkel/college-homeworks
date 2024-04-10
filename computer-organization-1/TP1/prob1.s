aee6ba68e7fc6e9d29beb309b8c0abfd9b876202
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-04-10 23:12:11 +0000

    feat(*): improve comments

--- computer-organization-1/TP1/prob1.s
+++ computer-organization-1/TP1/prob1.s
@@ -31,13 +31,13 @@ teste2: la a0, vetor
 ##### R2 START MODIFIQUE AQUI START #####
 multiplos:  addi a3, zero, zero             # quantidade de items checados no vetor
             addi r0, zero, zero             # quantidade de múltiplos
-check:      ld a5, 0(a0)                    # elemento atual do vetor que será checado
-            rem a4, a5, a2                  # resto da divisao do elemento pelo verificador
-            bne a4, zero, next              # caso nao for múltiplo, evita incremento na quantidade de múltiplos
-            add r0, r0, a4                  # incrementa resultado do mod na quantidade de múltiplos
+check:      ld a5, 0(a0)                    # busca elemento atual do vetor que será checado
+            rem a4, a5, a2                  # verifica resto da divisao do elemento verificado
+            bne a4, zero, next              # caso o elemento não for múltiplo, evita incremento na quantidade de múltiplos
+            addi r0, r0, 1                  # incrementa quantidade de múltiplos
 next:       addi a0, a0, 8                  # avança uma posição do vetor
             addi a3, a3, 1                  # incrementa a quantidade de items checados
-            blt a3, a1, check               # caso não tiver checado todos os items do vetor, continua checagem
+            bne a3, a1, check               # caso não tiver checado todos os items do vetor, segue para a checagem do proximo item
             jalr zero, 0(ra)
 ##### R2 END MODIFIQUE AQUI END #####
 
