eb73526cb75337c57279e5bdba9ef40b865b8d45
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-04-15 21:32:52 +0000

    chore(*): sort files

--- algorithms-1/TP0/src/app.cpp
+++ algorithms-1/TP0/src/app.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include "quick-sort-recursive.hpp"
 #include "quick-sort-iterative.hpp"
+#include "fibonacci-recursive.hpp"
+#include "fibonacci-iterative.hpp"
 
-int main() {
+void testQuicksortAlgorithms () {
     std::vector<int> items = {5, 1, 4, 8, 7, 2, 3, 6};
 
     std::vector<int> sortedItems = QuicksortRecursivoPuro(items);
@@ -16,6 +18,16 @@ int main() {
         std::cout << sortedItems[i] << " ";
     }
     std::cout << std::endl;
+}
+
+void testFibonacciAlgorithms () {
+    fibonacciIterative(10);
+    fibonacciRecursive(10);
+}
+
+int main() {
+    testQuicksortAlgorithms();
+    testFibonacciAlgorithms();
 
     return 0;
 }
