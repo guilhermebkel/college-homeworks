55e3ab57ebedcbb0600ff641cc179dfe75663505
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-04-22 23:27:04 +0000

    feat(*): fix invalid offset

--- computer-organization-1/TP1/prob2.s
+++ computer-organization-1/TP1/prob2.s
@@ -40,7 +40,7 @@ media:          addi sp, sp, -12                # aloca espaço para 3 variávei
 sum:            lw  a3, 0(a0)                   # a3: elemento atual
                 add, a2, a2, a3                 # soma elemento atual na soma total
                 addi a1, a1, -1                 # decrementa quantidade de elementos processados
-                addi a0, a0, 4                  # avança uma posição do vetor (32 bits / 8 = 4, entao incrementa de 4 em 4) 
+                addi a0, a0, 4                  # avança uma posição do vetor 
                 bne a1, zero, sum               # caso tiver mais elementos para processar, prossegue para próxima soma
                 lw  a1, 0(sp)                   # recupera snapshot de a1 (tamanho do vetor)
                 div a0, a2, a1                  # calcula a média
@@ -61,19 +61,21 @@ mult:           add a7, a0, zero                # a7: referência temporaria do 
                 jal ra, media                   # calcula media dos valores do vetor 1
                 add a5, a0, zero                # a5: media dos valores do vetor 1
                 add a0, a7, zero                # retorna referência do vetor 1
-                lw a6, 0(a0)                    # a6: elemento do vetor 1
+                slli a7, a4, 2                  # a7: offset do elemento atual do vetor
+                add a7, a0, a7                  # a7: endereço do elemento atual do vetor 1
+                lw a6, 0(a7)                    # a6: elemento atual do vetor 1
                 sub a6, a6, a5                  # a6: Xi - media(X)
                 add a0, a2, zero                # adiciona vetor 2 como argumento do procedimento de media
                 jal ra, media                   # calcula media dos valores do vetor 2
                 add a5, a0, zero                # a5: media dos valores do vetor 2
                 add a0, a7, zero                # retorna referência do vetor 1
-                lw a7, 0(a2)                    # a7: elemento do vetor 2
+                slli a7, a4, 2                  # a7: offset do elemento atual do vetor
+                add a7, a2, a7                  # a7: endereço do elemento atual do vetor 2
+                lw a7, 0(a7)                    # a7: elemento atual do vetor 2
                 sub a7, a7, a5                  # a7: Yi - media(Y)
                 mul a6, a6, a7                  # a6: (Xi - media(X)) * (Yi - media(Y))
                 add a3, a3, a6                  # soma produto da diferença atual na soma total
                 addi a4, a4, 1                  # incrementa quantidade de elementos processados
-                addi a0, a0, 4                  # avança uma posição do vetor 1 (32 bits / 8 = 4, entao incrementa de 4 em 4) 
-                addi a2, a2, 4                  # avança uma posição do vetor 2 (32 bits / 8 = 4, entao incrementa de 4 em 4) 
                 bne a1, a4, mult                # caso tiver mais elementos para processar, prossegue para próxima multiplicação
                 addi a7, a1, -1                 # a7: N - 1
                 div a0, a3, a7                  # calcula a covariância
