a055ef248cdf9610d688ac0cc885d7a2bf5450aa
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-04-22 23:11:25 +0000

    chore(*): fix business rules

--- computer-organization-1/TP1/prob2.s
+++ computer-organization-1/TP1/prob2.s
@@ -2,13 +2,19 @@
 
 ##### R1 START MODIFIQUE AQUI START #####
 
-vetor1: .word 1 2 3 4 #Primeiro vetor
-vetor2: .word 1 1 1 1 #Segundo vetor
+vetor1: .word 1 2 #Primeiro vetor
+vetor2: .word 1 1 #Segundo vetor
 
 ##### R1 END MODIFIQUE AQUI END #####
       
 .text    
 
+        add s0, zero, zero
+        la a0, vetor1
+        la a2, vetor2
+        addi a1, zero, 2
+        jal ra, covariancia
+        beq zero, zero, FIM
         add s0, zero, zero
         la a0, vetor1
         addi a1, zero, 4
@@ -42,13 +48,15 @@ sum:            lw  a3, 0(a0)                   # a3: elemento atual
                 lw  a3, 8(sp)                   # recupera snapshot de a3
                 addi sp, sp, 12                 # libera espaço das 3 variáveis na pilha
                 jalr zero, 0(ra)                # retorna do procedimento
-covariancia:    addi sp, sp, -20                # aloca espaço para 5 variáveis na pilha
-                sw  a1, 0(sp)                   # salva snapshot de a1 (tamanho dos vetores)
+covariancia:    addi sp, sp, -24                # aloca espaço para 6 variáveis na pilha
+                sw  ra, 0(sp)                   # salva snapshot de ra (retorno do procedimento de covariancia)
                 sw  a3, 4(sp)                   # salva snapshot de a3
-                sw  a5, 8(sp)                   # salva snapshot de a5
-                sw  a6, 12(sp)                  # salva snapshot de a6
-                sw  a7, 16(sp)                  # salva snapshot de a7
+                sw  a4, 8(sp)                   # salva snapshot de a4
+                sw  a5, 12(sp)                  # salva snapshot de a5
+                sw  a6, 16(sp)                  # salva snapshot de a6
+                sw  a7, 20(sp)                  # salva snapshot de a7
                 add a3, zero, zero              # a3: soma dos produtos da diferença
+                add a4, zero, zero              # a4: quantidade de elementos processados
 mult:           add a7, a0, zero                # a7: referência temporaria do vetor 1
                 jal ra, media                   # calcula media dos valores do vetor 1
                 add a5, a0, zero                # a5: media dos valores do vetor 1
@@ -63,18 +71,19 @@ mult:           add a7, a0, zero                # a7: referência temporaria do 
                 sub a7, a7, a5                  # a7: Yi - media(Y)
                 mul a6, a6, a7                  # a6: (Xi - media(X)) * (Yi - media(Y))
                 add a3, a3, a6                  # soma produto da diferença atual na soma total
-                addi a1, a1, -1                 # decrementa quantidade de elementos processados
+                addi a4, a4, 1                  # incrementa quantidade de elementos processados
                 addi a0, a0, 4                  # avança uma posição do vetor 1 (32 bits / 8 = 4, entao incrementa de 4 em 4) 
                 addi a2, a2, 4                  # avança uma posição do vetor 2 (32 bits / 8 = 4, entao incrementa de 4 em 4) 
-                bne a1, zero, mult              # caso tiver mais elementos para processar, prossegue para próxima multiplicação
-                lw  a1, 0(sp)                   # recupera snapshot de a1 (tamanho dos vetores)
+                bne a1, a4, mult                # caso tiver mais elementos para processar, prossegue para próxima multiplicação
                 addi a7, a1, -1                 # a7: N - 1
                 div a0, a3, a7                  # calcula a covariância
+                lw  ra, 0(sp)                   # recupera snapshot de ra (retorno do procedimento de covariancia)
                 lw  a3, 4(sp)                   # recupera snapshot de a3
-                lw  a5, 8(sp)                   # recupera snapshot de a5
-                lw  a6, 12(sp)                  # recupera snapshot de a6
-                lw  a7, 16(sp)                  # recupera snapshot de a7
-                addi sp, sp, 20                 # libera espaço das 5 variáveis na pilha
+                lw  a4, 8(sp)                   # recupera snapshot de a4
+                lw  a5, 12(sp)                  # recupera snapshot de a5
+                lw  a6, 16(sp)                  # recupera snapshot de a6
+                lw  a7, 20(sp)                  # recupera snapshot de a7
+                addi sp, sp, 24                 # libera espaço das 6 variáveis na pilha
                 jalr zero, 0(ra)                # retorna do procedimento
 
 ##### R2 END MODIFIQUE AQUI END #####
