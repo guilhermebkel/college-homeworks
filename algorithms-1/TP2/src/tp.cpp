a25836d6a5a3a7b1322cffed72f2c6580f73c9ac
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-07-19 21:52:02 +0000

    feat(*): add base structure

--- algorithms-1/TP2/src/tp.cpp
+++ algorithms-1/TP2/src/tp.cpp
@@ -1,164 +1,98 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <climits> // Para incluir INT_MAX
 
-struct Vertex;
-
-struct Edge {
-    Vertex *fromVertex;
-    Vertex *toVertex;
-    double angle;
-    bool visited;
-    Edge *nextEdge;
-    Edge *reverseEdge;
-};
+using namespace std;
 
 struct Vertex {
-    int id;
-    int degree;
-    std::vector<int> neighborVerticesIds;
-    double x, y;
-    std::vector<Edge*> outboundEdges;
+    int d; // distância mínima em termos de tempo
+    int hops; // número de saltos
+    int pi; // predecessor
+    Vertex() : d(INT_MAX), hops(INT_MAX), pi(-1) {}
 };
 
-struct Input {
-    std::vector<Vertex> vertices;
-    std::vector<std::pair<int, int>> edgesByVertices;
+struct Edge {
+    int to;
+    int year;
+    int travel_time;
+    int cost;
 };
 
-void addEdgeToVertex(Vertex* vertex, Edge *edge) {
-    vertex->outboundEdges.push_back(edge);
-}
+using Graph = vector<vector<Edge>>;
 
-void sortVertexEdges(Vertex* vertex) {
-    std::sort(vertex->outboundEdges.begin(), vertex->outboundEdges.end(), [](Edge *e1, Edge *e2) {
-        return e1->angle < e2->angle;
-    });
+void earliest_arrival_bfs_explore(const Graph& G, vector<Vertex>& V, int r, int t) {
+    queue<int> Q;
+    Q.push(r);
 
-    Edge *previousEdge = vertex->outboundEdges.back();
+    while (!Q.empty()) {
+        int u = Q.front();
+        Q.pop();
 
-    for (Edge *edge : vertex->outboundEdges) {
-        edge->nextEdge = previousEdge;
-        previousEdge = edge;
-    }
-}
-
-void setReverseEdge(Edge *edge1, Edge *edge2) {
-    edge1->reverseEdge = edge2;
-    edge2->reverseEdge = edge1;
-};
-
-Edge *createEdge(Vertex *fromVertex, Vertex *toVertex) {
-    return new Edge {
-        .fromVertex = fromVertex,
-        .toVertex = toVertex,
-        .angle = atan2(toVertex->y - fromVertex->y, toVertex->x - fromVertex->x),
-        .visited = false,
-        .nextEdge = nullptr,
-        .reverseEdge = nullptr
-    };
-};
-
-void printResult(std::vector<std::vector<Edge*>> faces) {
-    std::cout << faces.size() << std::endl;
-
-    for (auto& face : faces) {
-        std::cout << face.size() + 1 << " ";
-
-        for (Edge *edge : face) {
-            std::cout << edge->fromVertex->id << " ";
-        }
-
-        std::cout << face.back()->toVertex->id << std::endl;
-    }
-}
-
-Input readInput () {
-    Input input;
-
-    int verticesCount, edgesCount;
-    std::cin >> verticesCount >> edgesCount;
-
-    for (int i = 0; i < verticesCount; i++) {
-        int vertexId = i + 1;
-
-        Vertex vertex;
-        vertex.id = vertexId;
-
-        std::cin >> vertex.x >> vertex.y >> vertex.degree;
-
-        for (int j = 0; j < vertex.degree; j++) {
-            int neighborVertexId;
-
-            std::cin >> neighborVertexId;
-
-            vertex.neighborVerticesIds.push_back(neighborVertexId);
-
-            if (vertexId < neighborVertexId) {
-                input.edgesByVertices.push_back(std::make_pair(vertexId, neighborVertexId));
+        for (const Edge& edge : G[u]) {
+            int v = edge.to;
+            if (edge.year >= t && V[v].d > V[u].d + edge.travel_time) {
+                V[v].d = V[u].d + edge.travel_time;
+                V[v].pi = u;
+                V[v].hops = V[u].hops + 1;
+                Q.push(v);
             }
         }
+    }
+}
 
-        input.vertices.push_back(vertex);
+vector<int> find_min_travel_times(const Graph& graph, int start, int tmin, int tmax) {
+    int n = graph.size();
+    vector<Vertex> V(n);
+
+    V[start].d = tmin;
+    V[start].hops = 0;
+    V[start].pi = -1;
+
+    for (int t = tmin; t <= tmax; ++t) {
+        for (int u = 0; u < n; ++u) {
+            if (V[u].d <= t) {
+                earliest_arrival_bfs_explore(graph, V, u, t);
+            }
+        }
     }
 
-    return input;
+    vector<int> min_times(n);
+    for (int i = 0; i < n; ++i) {
+        min_times[i] = V[i].d == INT_MAX ? -1 : V[i].d;
+    }
+
+    return min_times;
 }
 
 int main() {
-    Input input = readInput();
+    int N, M;
+    cin >> N >> M;
 
-    std::vector<Vertex*> vertices;
-    std::vector<std::pair<int, int>> edgesByVertices = input.edgesByVertices;
+    Graph graph(N);
 
-    for (auto& vertex : input.vertices) {
-        vertices.push_back(&vertex);
+    for (int i = 0; i < M; ++i) {
+        int u, v, a, l, c;
+        cin >> u >> v >> a >> l >> c;
+        --u; --v; // assumindo que os vértices de entrada são 1-based
+        graph[u].push_back({v, a, l, c});
+        graph[v].push_back({u, a, l, c});
     }
 
-    std::vector<Edge*> edges;
+    int start = 0; // assumindo que o palácio está no vértice 0
+    int tmin = 0;  // tempo inicial mínimo
+    int tmax = 1000; // tempo máximo (ou um valor apropriado para o seu caso)
+    
+    vector<int> min_times = find_min_travel_times(graph, start, tmin, tmax);
 
-    for (auto& edgeByVertice : edgesByVertices) {
-        Vertex *toVertex = vertices[edgeByVertice.second - 1];
-        Vertex *fromVertex = vertices[edgeByVertice.first - 1];
-
-        Edge *toEdge = createEdge(toVertex, fromVertex);
-        Edge *fromEdge = createEdge(fromVertex, toVertex);
-
-        edges.push_back(toEdge);
-        edges.push_back(fromEdge);
-
-        setReverseEdge(toEdge, fromEdge);
-
-        addEdgeToVertex(toVertex, toEdge);
-        addEdgeToVertex(fromVertex, fromEdge);
-    }
-
-    for (auto& vertex : vertices) {
-        sortVertexEdges(vertex);
-    }
-
-    std::vector<std::vector<Edge*>> faces;
-
-    for (auto& edge : edges) {
-        if (edge->visited) {
-            continue;
+    for (int i = 0; i < N; ++i) {
+        if (min_times[i] == -1) {
+            cout << "INF" << endl;
+        } else {
+            cout << min_times[i] << endl;
         }
-
-        std::vector<Edge*> face;
-
-        Edge *computedEdge = edge;
-
-        do {
-            face.push_back(computedEdge);
-            computedEdge->visited = true;
-            computedEdge = computedEdge->reverseEdge->nextEdge;
-        } while (computedEdge != edge);
-
-        faces.push_back(face);
     }
-
-    printResult(faces);
 
     return 0;
 }
