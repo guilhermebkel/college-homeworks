ab41a536f24de3da427bf24b05de2c32468149ca
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-07-28 15:25:43 +0000

    chore(*): merge functions

--- algorithms-1/TP2/src/tp.cpp
+++ algorithms-1/TP2/src/tp.cpp
@@ -5,8 +5,7 @@
 #include <climits>
 #include <algorithm>
 #include <functional>
-#include <set> // Para armazenar anos únicos
-#include <numeric> // Para iota
+#include <set>
 
 using namespace std;
 
@@ -25,28 +24,44 @@ struct Edge {
     int cost;
 };
 
-using Graph = vector<vector<Edge>>;
+struct UnionFind {
+    vector<int> parent, rank;
+    int components;
 
-// Função para explorar o grafo considerando o tempo
-void earliest_arrival_bfs_explore(const Graph& G, vector<Vertex>& V, int r) {
-    queue<int> Q;
-    Q.push(r);
+    UnionFind(int n) : parent(n), rank(n, 0), components(n) {
+         for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+         }
+    }
 
-    while (!Q.empty()) {
-        int u = Q.front();
-        Q.pop();
+    int find(int u) {
+        if (u != parent[u]) {
+            parent[u] = find(parent[u]);
+        }
 
-        for (const Edge& edge : G[u]) {
-            int v = edge.to;
-            if (V[v].d > V[u].d + edge.travel_time) {
-                V[v].d = V[u].d + edge.travel_time;
-                V[v].pi = u;
-                V[v].hops = V[u].hops + 1;
-                Q.push(v);
+        return parent[u];
+    }
+
+    void unite(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+
+        if (rootU != rootV) {
+            if (rank[rootU] > rank[rootV]) {
+                parent[rootV] = rootU;
+            } else if (rank[rootU] < rank[rootV]) {
+                parent[rootU] = rootV;
+            } else {
+                parent[rootV] = rootU;
+                rank[rootU]++;
             }
+
+            components--;
         }
     }
-}
+};
+
+using Graph = vector<vector<Edge>>;
 
 // Função para encontrar os tempos mínimos de viagem
 vector<int> find_min_travel_times(const Graph& graph, int start) {
@@ -56,7 +71,23 @@ vector<int> find_min_travel_times(const Graph& graph, int start) {
     V[start].hops = 0;
     V[start].pi = -1;
 
-    earliest_arrival_bfs_explore(graph, V, start);
+    queue<int> Q;
+    Q.push(start);
+
+    while (!Q.empty()) {
+        int u = Q.front();
+        Q.pop();
+
+        for (const Edge& edge : graph[u]) {
+            int v = edge.to;
+            if (V[v].d > V[u].d + edge.travel_time) {
+                V[v].d = V[u].d + edge.travel_time;
+                V[v].pi = u;
+                V[v].hops = V[u].hops + 1;
+                Q.push(v);
+            }
+        }
+    }
 
     vector<int> min_times(n);
 
@@ -67,102 +98,50 @@ vector<int> find_min_travel_times(const Graph& graph, int start) {
     return min_times;
 }
 
-// Função de união-find com path compression e union by rank
-struct UnionFind {
-    vector<int> parent, rank;
-    int components;
-
-    UnionFind(int n) : parent(n), rank(n, 0), components(n) {
-        iota(parent.begin(), parent.end(), 0);
-    }
-
-    int find(int u) {
-        if (u != parent[u])
-            parent[u] = find(parent[u]);
-        return parent[u];
-    }
-
-    void unite(int u, int v) {
-        int rootU = find(u);
-        int rootV = find(v);
-        if (rootU != rootV) {
-            if (rank[rootU] > rank[rootV]) {
-                parent[rootV] = rootU;
-            } else if (rank[rootU] < rank[rootV]) {
-                parent[rootU] = rootV;
-            } else {
-                parent[rootV] = rootU;
-                rank[rootU]++;
-            }
-            components--;
-        }
-    }
-};
-
-// Função para verificar se todos os vértices são alcançáveis até o ano t
-bool is_all_reachable(const Graph& graph, int start, int t, UnionFind& uf, const vector<Edge>& edges) {
-    for (const Edge& edge : edges) {
-        if (edge.year <= t) {
-            uf.unite(edge.from, edge.to);
-        }
-    }
-    return uf.components == 1;
-}
-
 // Função para encontrar o ano em que todos os vértices são alcançáveis
 int find_year_all_reachable(const Graph& graph, int start, const set<int>& years, const vector<Edge>& edges) {
-    UnionFind uf(graph.size());
+   auto is_all_reachable = [&](int t) {
+        UnionFind uf(graph.size());
+
+        for (const Edge& edge : edges) {
+            if (edge.year <= t) {
+                uf.unite(edge.from, edge.to);
+            }
+        }
+
+        return uf.components == 1;
+    };
+
     auto it = years.begin();
     int tmin = *it;
     int tmax = *(--years.end());
 
     while (tmin < tmax) {
         int mid = tmin + (tmax - tmin) / 2;
-        UnionFind temp_uf = uf; // Copiamos o estado atual do UnionFind para manter as mudanças locais
-        if (is_all_reachable(graph, start, mid, temp_uf, edges)) {
+
+        if (is_all_reachable(mid)) {
             tmax = mid;
         } else {
             tmin = mid + 1;
         }
     }
 
-    UnionFind final_uf = uf;
-    return is_all_reachable(graph, start, tmin, final_uf, edges) ? tmin : -1;
+    return is_all_reachable(tmin) ? tmin : -1;
 }
 
 // Função para encontrar o menor custo necessário para conectar todo o reino (MST)
 int find_min_cost_mst(int N, vector<Edge>& edges) {
-    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
+     sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
         return a.cost < b.cost;
     });
 
-    vector<int> parent(N), rank(N, 0);
-    for (int i = 0; i < N; ++i) parent[i] = i;
-
-    function<int(int)> find = [&](int u) {
-        if (u != parent[u]) parent[u] = find(parent[u]);
-        return parent[u];
-    };
-
-    auto unite = [&](int u, int v) {
-        int rootU = find(u);
-        int rootV = find(v);
-        if (rootU != rootV) {
-            if (rank[rootU] > rank[rootV]) {
-                parent[rootV] = rootU;
-            } else if (rank[rootU] < rank[rootV]) {
-                parent[rootU] = rootV;
-            } else {
-                parent[rootV] = rootU;
-                rank[rootU]++;
-            }
-        }
-    };
+    UnionFind uf(N);
 
     int min_cost = 0;
+
     for (const Edge& edge : edges) {
-        if (find(edge.from) != find(edge.to)) {
-            unite(edge.from, edge.to);
+        if (uf.find(edge.from) != uf.find(edge.to)) {
+            uf.unite(edge.from, edge.to);
             min_cost += edge.cost;
         }
     }
@@ -175,6 +154,7 @@ bool has_path_with_n_minus_1_edges(const Graph& graph, int u, int target, int ed
     if (edges == 0) {
         return u == target;
     }
+
     visited[u] = true;
 
     for (const Edge& edge : graph[u]) {
@@ -186,6 +166,7 @@ bool has_path_with_n_minus_1_edges(const Graph& graph, int u, int target, int ed
     }
 
     visited[u] = false;
+
     return false;
 }
 
@@ -194,7 +175,7 @@ int find_year_all_mutually_reachable(const Graph& graph, const set<int>& years, 
     int n = graph.size();
     vector<bool> visited(n, false);
     
-    auto it = years.lower_bound(tmin); // Encontrar o menor valor em years >= tmin
+    auto it = years.lower_bound(tmin);
     
     while (it != years.end()) {
         int year = *it;
@@ -203,7 +184,8 @@ int find_year_all_mutually_reachable(const Graph& graph, const set<int>& years, 
         }
         ++it;
     }
-    return -1; // Se não houver ano que satisfaça a condição
+
+    return -1;
 }
 
 int main() {
