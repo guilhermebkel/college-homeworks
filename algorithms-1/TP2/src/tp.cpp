2b5f499a15d6963d52ee95ab6bfb55788fa77de3
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-07-30 13:14:36 +0000

    feat(*): add full refactor

--- algorithms-1/TP2/src/tp.cpp
+++ algorithms-1/TP2/src/tp.cpp
@@ -5,194 +5,182 @@
 #define INF 0x3f3f3f3f
 
 class DisjointSetUnion {
-    private:
-        std::vector<long long int> parent;
-        std::vector<long long int> rank;
-    
-    public:
-        DisjointSetUnion(long long int n) {
-            for (long long int i = 0; i < n; i++) { 
-                this->parent.push_back(-1);
-                this->rank.push_back(1);
-            }
-        }
+	private:
+		std::vector<long long int> parent;
+		std::vector<long long int> rank;
+	
+	public:
+		DisjointSetUnion(long long int n) {
+			for (long long int i = 0; i < n; i++) { 
+				this->parent.push_back(-1);
+				this->rank.push_back(1);
+			}
+		}
 
-        long long int find(long long int v) {
-            if (parent[v] == -1) 
-                return v; 
-        
-            return parent[v] = find(parent[v]); 
-        }
+		long long int find(long long int v) {
+			if (parent[v] == -1) {
+				return v;
+			}
+	
+			return parent[v] = find(parent[v]); 
+		}
 
-        void unite(long long int u, long long int v) {
-            long long int s1 = this->find(u); 
-            long long int s2 = this->find(v); 
+		void unite(long long int u, long long int v) {
+			long long int s1 = this->find(u); 
+			long long int s2 = this->find(v); 
 
-            if (s1 != s2) { 
-                if (rank[s1] < rank[s2]) { 
-                    parent[s1] = s2; 
-                } 
-                else if (rank[s1] > rank[s2]) { 
-                    parent[s2] = s1; 
-                } 
-                else { 
-                    parent[s2] = s1; 
-                    rank[s1] += 1; 
-                } 
-            } 
-        }
+			if (s1 != s2) { 
+				if (rank[s1] < rank[s2]) { 
+					parent[s1] = s2; 
+				} else if (rank[s1] > rank[s2]) { 
+					parent[s2] = s1; 
+				} else { 
+					parent[s2] = s1; 
+					rank[s1] += 1; 
+				}
+			} 
+		}
 };
 
 class Edge {
-    public:
-        long long int v, u;
-        long long int year;
-        long long int duration;
-        long long int cost;
-        Edge(long long int _v, long long int _u, long long int _year, long long int _duration, long long int _cost) {
-            this->v = _v;
-            this->u = _u;
-            this->year = _year;
-            this->duration = _duration;
-            this->cost = _cost;
-        }
+	public:
+		long long int fromVertexId, toVertexId, year, duration, cost;
+
+		Edge(long long int fromVertexId, long long int toVertexId, long long int year, long long int duration, long long int cost) {
+			this->fromVertexId = fromVertexId;
+			this->toVertexId = toVertexId;
+			this->year = year;
+			this->duration = duration;
+			this->cost = cost;
+		}
 };
 
 class Vertex {
-    public:
-        long long int distance, id, totalTime;
-        std::vector<Edge> adjList;
+	public:
+		long long int id, distance, totalTime;
+		std::vector<Edge> adjacencyList;
 
-        Vertex(long long int _id){
-            this->distance = INF;
-            this->totalTime = 0;
-            this->id = _id;
-        }
+		Vertex(long long int id){
+			this->id = id;
+			this->distance = INF;
+			this->totalTime = 0;
+		}
 
-        void addEdge(Edge e) {
-            this->adjList.push_back(e);  
-        }
+		void addEdge(Edge edge) {
+			this->adjacencyList.push_back(edge);  
+		}
 };
 
 using Graph = std::vector<Vertex>;
 
-using iPair = std::pair<long long int, long long int>;
+using DistanceAndIdPair = std::pair<long long int, long long int>;
 
 void dijkstraAlgorithm(Graph graph) {
-    int verticesCount = graph.size();
-    std::priority_queue<iPair, std::vector<iPair>, std::greater<iPair>> priorityQueue;
+	int verticesCount = graph.size();
+	std::priority_queue<DistanceAndIdPair, std::vector<DistanceAndIdPair>, std::greater<DistanceAndIdPair>> priorityQueue;
 
-    priorityQueue.push(std::make_pair(0, 1));
-    graph[0].distance = 0;
+	priorityQueue.push(std::make_pair(0, 1));
+	graph[0].distance = 0;
 
-    int bigger_time = 0;
+	while (!priorityQueue.empty()) {
+		long long int fromVertexId = priorityQueue.top().second;
+		Vertex *fromVertex = &graph[fromVertexId-1];
+		priorityQueue.pop();
 
-    while (!priorityQueue.empty()) {
-        long long int u = priorityQueue.top().second;
-        Vertex *vertex_u = &graph[u-1];
-        priorityQueue.pop();
+		for(size_t i=0; i < fromVertex->adjacencyList.size(); i++) {
+			Vertex *toVertex = &graph[fromVertex->adjacencyList[i].toVertexId-1];
 
-        for(long long int i=0; i < vertex_u->adjList.size(); i++) {
-            Vertex *vertex_v = &graph[vertex_u->adjList[i].u-1];
+			if (toVertex->distance > fromVertex->distance + fromVertex->adjacencyList[i].duration) {
+				toVertex->distance = fromVertex->distance + fromVertex->adjacencyList[i].duration;
+				toVertex->totalTime = fromVertex->adjacencyList[i].year;
 
-            if (vertex_v->distance > vertex_u->distance + vertex_u->adjList[i].duration) {
-                vertex_v->distance = vertex_u->distance + vertex_u->adjList[i].duration;
-                vertex_v->totalTime = vertex_u->adjList[i].year;
+				priorityQueue.push(std::make_pair(toVertex->distance, toVertex->id));
+			} else if (toVertex->distance == fromVertex->distance + fromVertex->adjacencyList[i].duration) {
+				if (toVertex->totalTime > fromVertex->adjacencyList[i].year) {
+					toVertex->distance = fromVertex->distance + fromVertex->adjacencyList[i].duration;
+					toVertex->totalTime = fromVertex->adjacencyList[i].year;
 
-                priorityQueue.push(std::make_pair(vertex_v->distance, vertex_v->id));
-            } else if (vertex_v->distance == vertex_u->distance + vertex_u->adjList[i].duration) {
-                if (vertex_v->totalTime > vertex_u->adjList[i].year) {
-                    vertex_v->distance = vertex_u->distance + vertex_u->adjList[i].duration;
-                    vertex_v->totalTime = vertex_u->adjList[i].year;
+					priorityQueue.push(std::make_pair(toVertex->distance, toVertex->id));
+				}
+			}
+		}
+	}
 
-                    priorityQueue.push(std::make_pair(vertex_v->distance, vertex_v->id));
-                }
-            }
-        }
-    }
+	for (long long int i = 0; i < verticesCount; i++) {
+		std::cout << graph[i].distance << std::endl;
+	}
 
-    for (long long int i = 0; i < verticesCount; i++) {
-        std::cout << graph[i].distance << std::endl;
-    }
+	long long int yearAllMutuallyReachable = 0;
 
-    long long int maxYear = 0;
+	for (long long int i = 0; i < verticesCount; i++) {
+		if (graph[i].totalTime > yearAllMutuallyReachable) {
+			yearAllMutuallyReachable = graph[i].totalTime;
+		}
+	}
 
-    for (long long int i = 0; i < verticesCount; i++) {
-        if (graph[i].totalTime > maxYear) {
-            maxYear = graph[i].totalTime;
-        }
-    }
-
-    std::cout << maxYear << std::endl;
+	std::cout << yearAllMutuallyReachable << std::endl;
 }
 
-void kruscalAlgorithm(Graph graph, bool type) {
-    int verticesCount = graph.size();
-    DisjointSetUnion dsu = DisjointSetUnion(verticesCount);
-    std::vector<Edge> edges;
-    long long int bigger_year = 0;
-    long long int ans = 0;
+void kruskalAlgorithm(Graph graph) {
+	int verticesCount = graph.size();
+	std::vector<Edge> edges;
 
-    for (long long int i = 0; i < verticesCount; i++) {
-        for (long long int j = 0; j < graph[i].adjList.size(); j++) {
-            edges.push_back(graph[i].adjList[j]);
-        }
-    }
+	for (long long int i = 0; i < verticesCount; i++) {
+		for (size_t j = 0; j < graph[i].adjacencyList.size(); j++) {
+			edges.push_back(graph[i].adjacencyList[j]);
+		}
+	}
+	
+	long long int yearAllReachable = 0;
+	DisjointSetUnion dsu1 = DisjointSetUnion(verticesCount);
+	std::sort(edges.begin(), edges.end(), [](Edge e1, Edge e2){ return e1.year < e2.year; });
+	for (size_t i = 0; i < edges.size(); i++) {
+		if (dsu1.find(edges[i].fromVertexId-1) != dsu1.find(edges[i].toVertexId-1)) {
+			dsu1.unite(edges[i].fromVertexId-1, edges[i].toVertexId-1);
 
-    if (type == 0) {
-        std::sort(edges.begin(), edges.end(), [](Edge e1, Edge e2){ return e1.year<e2.year; });
-    }
-    else if (type == 1) {
-        std::sort(edges.begin(), edges.end(), [](Edge e1, Edge e2){ return e1.cost<e2.cost; });
-    }
+			if (edges[i].year > yearAllReachable) {
+				yearAllReachable = edges[i].year;
+			}
+		}
+	}
+	std::cout << yearAllReachable << std::endl;
 
-    for (long long int i = 0; i < edges.size(); i++) {
-        if (dsu.find(edges[i].v-1) != dsu.find(edges[i].u-1)) {
-            dsu.unite(edges[i].v-1, edges[i].u-1);
+	long long int minCost = 0;
+	DisjointSetUnion dsu2 = DisjointSetUnion(verticesCount);
+	std::sort(edges.begin(), edges.end(), [](Edge e1, Edge e2){ return e1.cost < e2.cost; });
+	for (size_t i = 0; i < edges.size(); i++) {
+		if (dsu2.find(edges[i].fromVertexId-1) != dsu2.find(edges[i].toVertexId-1)) {
+			dsu2.unite(edges[i].fromVertexId-1, edges[i].toVertexId-1);
 
-            if (type == 0) {
-                if (edges[i].year > bigger_year) {
-                    bigger_year = edges[i].year;
-                }
-            } else if (type == 1) {
-                ans += edges[i].cost;
-            }
-        }
-    }
-    
-    if(type == 0) {
-        std::cout << bigger_year << std::endl;
-    } else if (type == 1) {
-        std::cout << ans << std::endl;
-    }
-    
+			minCost += edges[i].cost;
+		}
+	}
+	std::cout << minCost << std::endl;
 }
 
 int main() {
-    Graph graph;
+	Graph graph;
 
-    int N, M;
-    std::cin >> N >> M;
+	int N, M;
+	std::cin >> N >> M;
 
-    for (long long int i=0; i < N; i++) {
-        Vertex vertex = Vertex(i+1);
-        graph.push_back(vertex);
-    }
+	for (long long int i=0; i < N; i++) {
+		Vertex vertex = Vertex(i+1);
+		graph.push_back(vertex);
+	}
 
-    for(long long int i = 0; i < M; i++) {
-        int from, to, year, travel_time, cost;
-        std::cin >> from >> to >> year >> travel_time >> cost;
+	for(long long int i = 0; i < M; i++) {
+		int fromVertexId, toVertexId, year, travelTime, cost;
+		std::cin >> fromVertexId >> toVertexId >> year >> travelTime >> cost;
 
-        Edge edge = Edge(from, to, year, travel_time, cost);
-        graph[from-1].addEdge(edge);
-        
-        Edge inverse_edge = Edge(to, from, year, travel_time, cost);
-        graph[to-1].addEdge(inverse_edge);
-    }
-    
-    dijkstraAlgorithm(graph);
+		Edge edge = Edge(fromVertexId, toVertexId, year, travelTime, cost);
+		graph[fromVertexId-1].addEdge(edge);
+		
+		Edge inverse_edge = Edge(toVertexId, fromVertexId, year, travelTime, cost);
+		graph[toVertexId-1].addEdge(inverse_edge);
+	}
+	
+	dijkstraAlgorithm(graph);
 
-    kruscalAlgorithm(graph, 0);
-
-    kruscalAlgorithm(graph, 1);
+	kruskalAlgorithm(graph);
 }
