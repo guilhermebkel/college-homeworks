da158a582ab843a8b308764bb59391f0e584a80b
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2024-07-30 12:27:46 +0000

    feat(*): add tp business rules

--- algorithms-1/TP2/src/tp.cpp
+++ algorithms-1/TP2/src/tp.cpp
@@ -1,249 +1,198 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
-#include <climits>
 #include <algorithm>
-#include <functional>
-#include <set>
-#include <stack>
+#define INF 0x3f3f3f3f
 
-using namespace std;
-
-struct Vertex {
-    int d; // distância mínima em termos de tempo
-    int hops; // número de saltos
-    int pi; // predecessor
-    Vertex() : d(INT_MAX), hops(INT_MAX), pi(-1) {}
-};
-
-struct Edge {
-    int from;
-    int to;
-    int year;
-    int travel_time;
-    int cost;
-};
-
-struct UnionFind {
-    vector<int> parent, rank;
-    int components;
-
-    UnionFind(int n) : parent(n), rank(n, 0), components(n) {
-         for (int i = 0; i < n; ++i) {
-            parent[i] = i;
-         }
-    }
-
-    int find(int u) {
-        if (u != parent[u]) {
-            parent[u] = find(parent[u]);
-        }
-
-        return parent[u];
-    }
-
-    void unite(int u, int v) {
-        int rootU = find(u);
-        int rootV = find(v);
-
-        if (rootU != rootV) {
-            if (rank[rootU] > rank[rootV]) {
-                parent[rootV] = rootU;
-            } else if (rank[rootU] < rank[rootV]) {
-                parent[rootU] = rootV;
-            } else {
-                parent[rootV] = rootU;
-                rank[rootU]++;
-            }
-
-            components--;
-        }
-    }
-};
-
-using Graph = vector<vector<Edge>>;
-
-// Função para encontrar os tempos mínimos de viagem
-vector<int> find_min_travel_times(const Graph& graph, int start) {
-    int n = graph.size();
-    vector<Vertex> V(n);
-    V[start].d = 0;
-    V[start].hops = 0;
-    V[start].pi = -1;
-
-    queue<int> Q;
-    Q.push(start);
-
-    while (!Q.empty()) {
-        int u = Q.front();
-        Q.pop();
-
-        for (const Edge& edge : graph[u]) {
-            int v = edge.to;
-            if (V[v].d > V[u].d + edge.travel_time) {
-                V[v].d = V[u].d + edge.travel_time;
-                V[v].pi = u;
-                V[v].hops = V[u].hops + 1;
-                Q.push(v);
-            }
-        }
-    }
-
-    vector<int> min_times(n);
-
-    for (int i = 0; i < n; ++i) {
-        min_times[i] = V[i].d;
-    }
-
-    return min_times;
-}
-
-// Função para encontrar o ano em que todos os vértices são alcançáveis
-int find_year_all_reachable(const Graph& graph, int start, const set<int>& years, const vector<Edge>& edges) {
-   auto is_all_reachable = [&](int t) {
-        UnionFind uf(graph.size());
-
-        for (const Edge& edge : edges) {
-            if (edge.year <= t) {
-                uf.unite(edge.from, edge.to);
-            }
-        }
-
-        return uf.components == 1;
-    };
-
-    auto it = years.begin();
-    int tmin = *it;
-    int tmax = *(--years.end());
-
-    while (tmin < tmax) {
-        int mid = tmin + (tmax - tmin) / 2;
-
-        if (is_all_reachable(mid)) {
-            tmax = mid;
-        } else {
-            tmin = mid + 1;
-        }
-    }
-
-    return is_all_reachable(tmin) ? tmin : -1;
-}
-
-// Função para encontrar o menor custo necessário para conectar todo o reino (MST)
-int find_min_cost_mst(int N, vector<Edge>& edges) {
-    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
-        return a.cost < b.cost;
-    });
-
-    UnionFind uf(N);
-
-    int min_cost = 0;
-
-    for (const Edge& edge : edges) {
-        if (uf.find(edge.from) != uf.find(edge.to)) {
-            uf.unite(edge.from, edge.to);
-            min_cost += edge.cost;
-        }
-    }
-
-    return min_cost;
-}
-
-bool has_path_through_all_vertices(const Graph& graph, int t) {
-    int n = graph.size();
-    vector<bool> visited(n, false);
-    stack<pair<int, int>> stack; // pair<vertex, count of visited vertices>
-
-    stack.push({0, 1}); // Starting from vertex 0, 1 vertex visited
-
-    while (!stack.empty()) {
-        auto current = stack.top();
-        int u = current.first;
-        int visited_count = current.second;
-
-        stack.pop();
-
-        if (visited_count == n) {
-            return true;
-        }
-
-        if (!visited[u]) {
-            visited[u] = true;
-        }
-
-        vector<Edge> edges = graph[u];
-        sort(edges.begin(), edges.end(), [&graph, &visited](const Edge& a, const Edge& b) {
-            int unvisited_a = count_if(graph[a.to].begin(), graph[a.to].end(), [&visited](const Edge& e) { return !visited[e.to]; });
-            int unvisited_b = count_if(graph[b.to].begin(), graph[b.to].end(), [&visited](const Edge& e) { return !visited[e.to]; });
-
-            if (unvisited_a == unvisited_b) {
-                return graph[a.to].size() > graph[b.to].size();
-            }
-
-            return unvisited_a > unvisited_b;
-        });
-
-        for (const Edge& edge : edges) {
-            if (edge.year <= t && !visited[edge.to]) {
-                stack.push({edge.to, visited_count + 1});
-            }
-        }
-    }
-
-    return false;
-}
-
-// Função para encontrar o ano em que existe um caminho que começa no vértice 0 e passa por todos os vértices do grafo
-int find_year_all_mutually_reachable(const Graph& graph, const set<int>& years, int tmin) {
-    vector<bool> visited(graph.size(), false);
+class DisjointSetUnion {
+    private:
+        std::vector<long long int> parent;
+        std::vector<long long int> rank;
     
-    auto it = years.lower_bound(tmin); // Encontrar o menor valor em years >= tmin
-    
-    while (it != years.end()) {
-        int year = *it;
-        if (has_path_through_all_vertices(graph, year)) {
-            return year;
+    public:
+        DisjointSetUnion(long long int n) {
+            for (long long int i = 0; i < n; i++) { 
+                this->parent.push_back(-1);
+                this->rank.push_back(1);
+            }
         }
-        ++it;
+
+        long long int find(long long int v) {
+            if (parent[v] == -1) 
+                return v; 
+        
+            return parent[v] = find(parent[v]); 
+        }
+
+        void unite(long long int u, long long int v) {
+            long long int s1 = this->find(u); 
+            long long int s2 = this->find(v); 
+
+            if (s1 != s2) { 
+                if (rank[s1] < rank[s2]) { 
+                    parent[s1] = s2; 
+                } 
+                else if (rank[s1] > rank[s2]) { 
+                    parent[s2] = s1; 
+                } 
+                else { 
+                    parent[s2] = s1; 
+                    rank[s1] += 1; 
+                } 
+            } 
+        }
+};
+
+class Edge {
+    public:
+        long long int v, u;
+        long long int year;
+        long long int duration;
+        long long int cost;
+        Edge(long long int _v, long long int _u, long long int _year, long long int _duration, long long int _cost) {
+            this->v = _v;
+            this->u = _u;
+            this->year = _year;
+            this->duration = _duration;
+            this->cost = _cost;
+        }
+};
+
+class Vertex {
+    public:
+        long long int distance, id, totalTime;
+        std::vector<Edge> adjList;
+
+        Vertex(long long int _id){
+            this->distance = INF;
+            this->totalTime = 0;
+            this->id = _id;
+        }
+
+        void addEdge(Edge e) {
+            this->adjList.push_back(e);  
+        }
+};
+
+using Graph = std::vector<Vertex>;
+
+using iPair = std::pair<long long int, long long int>;
+
+void dijkstraAlgorithm(Graph graph) {
+    int verticesCount = graph.size();
+    std::priority_queue<iPair, std::vector<iPair>, std::greater<iPair>> priorityQueue;
+
+    priorityQueue.push(std::make_pair(0, 1));
+    graph[0].distance = 0;
+
+    int bigger_time = 0;
+
+    while (!priorityQueue.empty()) {
+        long long int u = priorityQueue.top().second;
+        Vertex *vertex_u = &graph[u-1];
+        priorityQueue.pop();
+
+        for(long long int i=0; i < vertex_u->adjList.size(); i++) {
+            Vertex *vertex_v = &graph[vertex_u->adjList[i].u-1];
+
+            if (vertex_v->distance > vertex_u->distance + vertex_u->adjList[i].duration) {
+                vertex_v->distance = vertex_u->distance + vertex_u->adjList[i].duration;
+                vertex_v->totalTime = vertex_u->adjList[i].year;
+
+                priorityQueue.push(std::make_pair(vertex_v->distance, vertex_v->id));
+            } else if (vertex_v->distance == vertex_u->distance + vertex_u->adjList[i].duration) {
+                if (vertex_v->totalTime > vertex_u->adjList[i].year) {
+                    vertex_v->distance = vertex_u->distance + vertex_u->adjList[i].duration;
+                    vertex_v->totalTime = vertex_u->adjList[i].year;
+
+                    priorityQueue.push(std::make_pair(vertex_v->distance, vertex_v->id));
+                }
+            }
+        }
     }
-    return -1; // Se não houver ano que satisfaça a condição
+
+    for (long long int i = 0; i < verticesCount; i++) {
+        std::cout << graph[i].distance << std::endl;
+    }
+
+    long long int maxYear = 0;
+
+    for (long long int i = 0; i < verticesCount; i++) {
+        if (graph[i].totalTime > maxYear) {
+            maxYear = graph[i].totalTime;
+        }
+    }
+
+    std::cout << maxYear << std::endl;
+}
+
+void kruscalAlgorithm(Graph graph, bool type) {
+    int verticesCount = graph.size();
+    DisjointSetUnion dsu = DisjointSetUnion(verticesCount);
+    std::vector<Edge> edges;
+    long long int bigger_year = 0;
+    long long int ans = 0;
+
+    for (long long int i = 0; i < verticesCount; i++) {
+        for (long long int j = 0; j < graph[i].adjList.size(); j++) {
+            edges.push_back(graph[i].adjList[j]);
+        }
+    }
+
+    if (type == 0) {
+        std::sort(edges.begin(), edges.end(), [](Edge e1, Edge e2){ return e1.year<e2.year; });
+    }
+    else if (type == 1) {
+        std::sort(edges.begin(), edges.end(), [](Edge e1, Edge e2){ return e1.cost<e2.cost; });
+    }
+
+    for (long long int i = 0; i < edges.size(); i++) {
+        if (dsu.find(edges[i].v-1) != dsu.find(edges[i].u-1)) {
+            dsu.unite(edges[i].v-1, edges[i].u-1);
+
+            if (type == 0) {
+                if (edges[i].year > bigger_year) {
+                    bigger_year = edges[i].year;
+                }
+            } else if (type == 1) {
+                ans += edges[i].cost;
+            }
+        }
+    }
+    
+    if(type == 0) {
+        std::cout << bigger_year << std::endl;
+    } else if (type == 1) {
+        std::cout << ans << std::endl;
+    }
+    
 }
 
 int main() {
+    Graph graph;
+
     int N, M;
-    cin >> N >> M;
-    Graph graph(N);
-    vector<Edge> edges;
-    set<int> years;
+    std::cin >> N >> M;
 
-    for (int i = 0; i < M; ++i) {
+    for (long long int i=0; i < N; i++) {
+        Vertex vertex = Vertex(i+1);
+        graph.push_back(vertex);
+    }
+
+    for(long long int i = 0; i < M; i++) {
         int from, to, year, travel_time, cost;
-        cin >> from >> to >> year >> travel_time >> cost;
+        std::cin >> from >> to >> year >> travel_time >> cost;
 
-        --from; --to;
-
-        graph[from].push_back({ .from = from, .to = to, .year = year, .travel_time = travel_time, .cost = cost });
-        graph[to].push_back({ .from = to, .to = from, .year = year, .travel_time = travel_time, .cost = cost });
-        edges.push_back({ .from = from, .to = to, .year = year, .travel_time = travel_time, .cost = cost });
-
-        years.insert(year);
+        Edge edge = Edge(from, to, year, travel_time, cost);
+        graph[from-1].addEdge(edge);
+        
+        Edge inverse_edge = Edge(to, from, year, travel_time, cost);
+        graph[to-1].addEdge(inverse_edge);
     }
+    
+    dijkstraAlgorithm(graph);
 
-    int start = 0; // assumindo que o palácio está no vértice 0
+    kruscalAlgorithm(graph, 0);
 
-    vector<int> min_times = find_min_travel_times(graph, start);
-    for (int i = 0; i < N; ++i) {
-        cout << min_times[i] << endl;
-    }
-
-    int year_all_reachable = find_year_all_reachable(graph, start, years, edges);
-    int year_all_mutually_reachable = find_year_all_mutually_reachable(graph, years, year_all_reachable);
-    int min_cost = find_min_cost_mst(N, edges);
-
-    cout << year_all_mutually_reachable << endl;
-    cout << year_all_reachable << endl;
-    cout << min_cost << endl;
-
-    return 0;
+    kruscalAlgorithm(graph, 1);
 }
