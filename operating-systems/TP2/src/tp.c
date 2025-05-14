77a2f6ea3d27d6e40f698e0892de6b32e3b98839
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2025-05-14 19:43:48 -0300

    chore(*): add base rules

--- operating-systems/TP2/src/tp.c
+++ operating-systems/TP2/src/tp.c
@@ -8,6 +8,19 @@ typedef enum {
     OPTIMAL
 } ReplacementAlgorithm;
 
+typedef struct {
+    ReplacementAlgorithm replacementAlgorithm;
+    char* traceFilePath;
+    unsigned pageSizeInKB;
+    unsigned memorySizeInKB;
+} AppConfig;
+
+typedef struct {
+    unsigned totalAccesses;
+    unsigned pageFaults;
+    unsigned dirtyPages;
+} TraceSimulationResult;
+
 ReplacementAlgorithm turnStringIntoReplacementAlgorithm(const char* str) {
     if (strcmp(str, "lru") == 0) {
         return LRU;
@@ -20,7 +33,7 @@ ReplacementAlgorithm turnStringIntoReplacementAlgorithm(const char* str) {
     }
 }
 
-unsigned calculateShiftBits(unsigned pageSizeInKB) {
+unsigned calculatePageShiftBits(unsigned pageSizeInKB) {
     unsigned s = 0;
     unsigned tmp = pageSizeInKB;
 
@@ -32,37 +45,61 @@ unsigned calculateShiftBits(unsigned pageSizeInKB) {
     return s;
 }
 
-// Função para processar o arquivo de trace
-void processTraceFile(const char* fileName, unsigned pageSizeInKB) {
+TraceSimulationResult processTraceFile(const char* fileName, unsigned pageSizeInKB) {
+    TraceSimulationResult traceSimulationResult = {0, 0, 0};
     FILE* file = fopen(fileName, "r");
 
     if (file == NULL) {
         printf("Erro ao abrir o arquivo.\n");
-        return;
+        return traceSimulationResult;
     }
 
-    unsigned s = calculateShiftBits(pageSizeInKB);
+    unsigned s = calculatePageShiftBits(pageSizeInKB);
     unsigned addr;
     char rw;
 
-    // Lendo o arquivo linha por linha
     while (fscanf(file, "%x %c", &addr, &rw) != EOF) {
+        traceSimulationResult.totalAccesses++;  // Contabiliza o acesso à memória
+
         // Calcular a página (descartar os s bits menos significativos do endereço)
         unsigned page = addr >> s;
 
-        printf("Endereço: %x, Operação: %c, Página: %x\n", addr, rw, page);
+        // Simular page fault e página suja
+        if (rw == 'W') {
+            traceSimulationResult.dirtyPages++;  // Página "suja" (escrita)
+        }
+
+        // Para simplificar, vamos assumir que a cada novo acesso a uma página ocorre um page fault
+        traceSimulationResult.pageFaults++;  // Assumindo que sempre há um page fault para esse exemplo
     }
 
     fclose(file);
+
+    return traceSimulationResult;
+}
+
+void generateTraceSimulationReport(const AppConfig* config, const TraceSimulationResult* traceSimulationResult) {
+    printf("Executando o simulador...\n");
+    printf("Arquivo de entrada: %s\n", config->traceFilePath);
+    printf("Tamanho da memória: %u KB\n", config->memorySizeInKB);
+    printf("Tamanho das páginas: %u KB\n", config->pageSizeInKB);
+    printf("Técnica de reposição: %s\n", 
+        (config->replacementAlgorithm == LRU) ? "lru" :
+        (config->replacementAlgorithm == FIFO) ? "fifo" : "optimal");
+    printf("Páginas lidas: %u\n", traceSimulationResult->totalAccesses);
+    printf("Páginas escritas: %u\n", traceSimulationResult->dirtyPages);
 }
 
 int main(int argc, char* argv[]) {
-    ReplacementAlgorithm replacementAlgorithm = turnStringIntoReplacementAlgorithm(argv[1]);
-    char* traceFilePath = argv[2];
-    int pageSizeInKB = atoi(argv[3]);
-    int memorySizeInKB = atoi(argv[4]);
+    AppConfig config;
+    config.replacementAlgorithm = turnStringIntoReplacementAlgorithm(argv[1]);
+    config.traceFilePath = argv[2];
+    config.pageSizeInKB = atoi(argv[3]);
+    config.memorySizeInKB = atoi(argv[4]);
 
-    processTraceFile(traceFilePath, pageSizeInKB);
+    TraceSimulationResult traceSimulationResult = processTraceFile(config.traceFilePath, config.pageSizeInKB);
+
+    generateTraceSimulationReport(&config, &traceSimulationResult);
 
     return 0;
 }
