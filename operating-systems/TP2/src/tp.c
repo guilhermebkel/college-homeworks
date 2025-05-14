eb88cc4b4a113fa9c381e94129e42f082efaa785
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2025-05-14 20:08:15 -0300

    chore(*): add correct base structure

--- operating-systems/TP2/src/tp.c
+++ operating-systems/TP2/src/tp.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <time.h>
 
 typedef enum {
     LRU,
     FIFO,
-    OPTIMAL
+    OPTIMAL, // não implementado ainda
+    RANDOM,
+    LFU
 } ReplacementAlgorithm;
 
 typedef struct {
@@ -19,7 +23,16 @@ typedef struct {
     unsigned totalAccesses;
     unsigned pageFaults;
     unsigned dirtyPages;
+    double executionTimeInSeconds;
 } TraceSimulationResult;
+
+typedef struct {
+    int pageNumber;
+    unsigned lastAccessTime;
+    unsigned accessCount;
+    int dirty;
+    unsigned loadTime;
+} Frame;
 
 ReplacementAlgorithm turnStringIntoReplacementAlgorithm(const char* str) {
     if (strcmp(str, "lru") == 0) {
@@ -28,8 +41,29 @@ ReplacementAlgorithm turnStringIntoReplacementAlgorithm(const char* str) {
         return FIFO;
     } else if (strcmp(str, "optimal") == 0) {
         return OPTIMAL;
+    } else if (strcmp(str, "random") == 0) {
+        return RANDOM;
+    } else if (strcmp(str, "lfu") == 0) {
+        return LFU;
     } else {
         return -1;
+    }
+}
+
+const char* getReplacementAlgorithmName(ReplacementAlgorithm replacementAlgorithm) {
+    switch (replacementAlgorithm) {
+        case LRU:
+            return "lru";
+        case FIFO:
+            return "fifo";
+        case RANDOM:
+            return "random";
+        case LFU:
+            return "lfu";
+        case OPTIMAL:
+            return "optimal (não implementado)";
+        default:
+            return "desconhecido";
     }
 }
 
@@ -45,35 +79,148 @@ unsigned calculatePageShiftBits(unsigned pageSizeInKB) {
     return s;
 }
 
-TraceSimulationResult processTraceFile(const char* fileName, unsigned pageSizeInKB) {
-    TraceSimulationResult traceSimulationResult = {0, 0, 0};
-    FILE* file = fopen(fileName, "r");
+int getPageToEvictByRandom(Frame* memory, unsigned numFrames) {
+    return rand() % numFrames;
+}
 
-    if (file == NULL) {
+int getPageToEvictByFIFO(Frame* memory, unsigned numFrames) {
+    int minLoadTime = INT_MAX;
+    int index = 0;
+    for (unsigned i = 0; i < numFrames; i++) {
+        if (memory[i].loadTime < minLoadTime) {
+            minLoadTime = memory[i].loadTime;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int getPageToEvictByLRU(Frame* memory, unsigned numFrames) {
+    int oldestTime = INT_MAX;
+    int index = 0;
+    for (unsigned i = 0; i < numFrames; i++) {
+        if (memory[i].lastAccessTime < oldestTime) {
+            oldestTime = memory[i].lastAccessTime;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int getPageToEvictByLFU(Frame* memory, unsigned numFrames) {
+    int leastAccessed = INT_MAX;
+    int index = 0;
+    for (unsigned i = 0; i < numFrames; i++) {
+        if (memory[i].accessCount < leastAccessed) {
+            leastAccessed = memory[i].accessCount;
+            index = i;
+        }
+    }
+    return index;
+}
+
+TraceSimulationResult simulateTrace(AppConfig appConfig) {
+    TraceSimulationResult traceSimulationResult = {0, 0, 0};
+    clock_t startTime = clock();
+    FILE* file = fopen(appConfig.traceFilePath, "r");
+
+    if (!file) {
         printf("Erro ao abrir o arquivo.\n");
         return traceSimulationResult;
     }
 
-    unsigned s = calculatePageShiftBits(pageSizeInKB);
+    srand(time(NULL)); // Random para RANDOM
+
+    unsigned s = calculatePageShiftBits(appConfig.pageSizeInKB) + 10; // converte de KB para bytes
     unsigned addr;
     char rw;
+    unsigned time = 0;
+
+    unsigned numFrames = appConfig.memorySizeInKB / appConfig.pageSizeInKB;
+    Frame* memory = malloc(sizeof(Frame) * numFrames);
+    for (unsigned i = 0; i < numFrames; i++) {
+        memory[i].pageNumber = -1;
+        memory[i].lastAccessTime = 0;
+        memory[i].accessCount = 0;
+        memory[i].dirty = 0;
+        memory[i].loadTime = 0;
+    }
 
     while (fscanf(file, "%x %c", &addr, &rw) != EOF) {
-        traceSimulationResult.totalAccesses++;  // Contabiliza o acesso à memória
+        traceSimulationResult.totalAccesses++;
+        time++;
 
-        // Calcular a página (descartar os s bits menos significativos do endereço)
         unsigned page = addr >> s;
+        int hit = 0;
 
-        // Simular page fault e página suja
-        if (rw == 'W') {
-            traceSimulationResult.dirtyPages++;  // Página "suja" (escrita)
+        // Verifica se a página já está na memória
+        for (unsigned i = 0; i < numFrames; i++) {
+            if (memory[i].pageNumber == page) {
+                memory[i].lastAccessTime = time;
+                memory[i].accessCount++;
+                if (rw == 'W') memory[i].dirty = 1;
+                hit = 1;
+                break;
+            }
         }
 
-        // Para simplificar, vamos assumir que a cada novo acesso a uma página ocorre um page fault
-        traceSimulationResult.pageFaults++;  // Assumindo que sempre há um page fault para esse exemplo
+        if (!hit) {
+            traceSimulationResult.pageFaults++;
+
+            // Verifica se há espaço livre
+            int placed = 0;
+
+            for (unsigned i = 0; i < numFrames; i++) {
+                if (memory[i].pageNumber == -1) {
+                    memory[i].pageNumber = page;
+                    memory[i].lastAccessTime = time;
+                    memory[i].accessCount = 1;
+                    memory[i].dirty = (rw == 'W') ? 1 : 0;
+                    memory[i].loadTime = time;
+                    placed = 1;
+                    break;
+                }
+            }
+
+            if (!placed) {
+                int evictIndex;
+
+                switch (appConfig.replacementAlgorithm) {
+                    case RANDOM:
+                        evictIndex = getPageToEvictByRandom(memory, numFrames);
+                        break;
+                    case FIFO:
+                        evictIndex = getPageToEvictByFIFO(memory, numFrames);
+                        break;
+                    case LRU:
+                        evictIndex = getPageToEvictByLRU(memory, numFrames);
+                        break;
+                    case LFU:
+                        evictIndex = getPageToEvictByLFU(memory, numFrames);
+                        break;
+                    default:
+                        evictIndex = 0;
+                        break;
+                }
+
+                if (memory[evictIndex].dirty) {
+                    traceSimulationResult.dirtyPages++;
+                }
+
+                memory[evictIndex].pageNumber = page;
+                memory[evictIndex].lastAccessTime = time;
+                memory[evictIndex].accessCount = 1;
+                memory[evictIndex].dirty = (rw == 'W') ? 1 : 0;
+                memory[evictIndex].loadTime = time;
+            }
+        }
     }
 
     fclose(file);
+    free(memory);
+
+    clock_t endTime = clock();  // termina temporizador
+    traceSimulationResult.executionTimeInSeconds = (double)(endTime - startTime) / CLOCKS_PER_SEC;
 
     return traceSimulationResult;
 }
@@ -83,23 +230,25 @@ void generateTraceSimulationReport(const AppConfig* config, const TraceSimulatio
     printf("Arquivo de entrada: %s\n", config->traceFilePath);
     printf("Tamanho da memória: %u KB\n", config->memorySizeInKB);
     printf("Tamanho das páginas: %u KB\n", config->pageSizeInKB);
-    printf("Técnica de reposição: %s\n", 
-        (config->replacementAlgorithm == LRU) ? "lru" :
-        (config->replacementAlgorithm == FIFO) ? "fifo" : "optimal");
-    printf("Páginas lidas: %u\n", traceSimulationResult->totalAccesses);
+    printf("Técnica de reposição: %s\n", getReplacementAlgorithmName(config->replacementAlgorithm));
+    printf("Páginas lidas: %u\n", traceSimulationResult->pageFaults);
     printf("Páginas escritas: %u\n", traceSimulationResult->dirtyPages);
+    printf("Total de acessos: %u\n", traceSimulationResult->totalAccesses);
+    printf("Tempo de execução: %.6f segundos\n", traceSimulationResult->executionTimeInSeconds);
+    printf("\n");
 }
 
 int main(int argc, char* argv[]) {
-    AppConfig config;
-    config.replacementAlgorithm = turnStringIntoReplacementAlgorithm(argv[1]);
-    config.traceFilePath = argv[2];
-    config.pageSizeInKB = atoi(argv[3]);
-    config.memorySizeInKB = atoi(argv[4]);
+    AppConfig appConfig;
 
-    TraceSimulationResult traceSimulationResult = processTraceFile(config.traceFilePath, config.pageSizeInKB);
+    appConfig.replacementAlgorithm = turnStringIntoReplacementAlgorithm(argv[1]);
+    appConfig.traceFilePath = argv[2];
+    appConfig.pageSizeInKB = atoi(argv[3]);
+    appConfig.memorySizeInKB = atoi(argv[4]);
 
-    generateTraceSimulationReport(&config, &traceSimulationResult);
+    TraceSimulationResult traceSimulationResult = simulateTrace(appConfig);
+
+    generateTraceSimulationReport(&appConfig, &traceSimulationResult);
 
     return 0;
 }
